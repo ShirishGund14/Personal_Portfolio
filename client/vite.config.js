f9f88faa3e8a53c8ee04251f45ef7a9e8374b279
Shirish Gund <shirish14g@gmail.com>
2024-06-07 00:32:01 +0530

    viteconfig updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Shirish_portfolio/'
+  base:'/Personal_Portfolio/'
 })
